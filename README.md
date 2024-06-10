1662919d0d7d3c2266cd7a8809d4b72597b52517
Sanjeev Kumar <sanjeevkumar761@gmail.com>
2024-06-10 20:18:33 +0000

    Initial files

---
+++ README.md
@@ -0,0 +1,3 @@
+Save .env-sample file as .env and update values of environment variables  
+python server.py  
+
---
+++ get_acss_details.py
@@ -0,0 +1,86 @@
+from promptflow import tool
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.subscription import SubscriptionClient
+from azure.mgmt.workloads import WorkloadsMgmtClient
+
+#import json
+#from bson.json_util import dumps
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@tool
+# Get all resource groups 
+# (here, we could also easily get all subs for a tenant, and go from there)
+
+def get_acss_details():
+    print("Getting ACSS Details")	
+    os.environ['AZURE_CLIENT_ID'] = os.getenv("AZURE_CLIENT_ID")
+    os.environ['AZURE_CLIENT_SECRET'] = os.getenv("AZURE_CLIENT_SECRET")
+    os.environ['AZURE_TENANT_ID'] = os.getenv("AZURE_TENANT_ID")
+    subId = os.getenv("AZURE_SUBSCRIPTION_ID")
+    rg_list = []
+
+    try:
+        # Get list of all VIS from the subscription and disentangle the resource groups
+        # For now, this is all we do. Further development will add more info.
+        client = WorkloadsMgmtClient(
+            credential=DefaultAzureCredential(),
+            subscription_id=subId)
+
+        response = client.sap_virtual_instances.list_by_subscription()
+
+        all_objects = list(response)
+        #print(all_objects)
+
+        for object in all_objects:
+            print("Obj--start")
+            print(object)
+            print("Obj--end")
+            #vis_info = object.name + ";" + object.environment + ";" + str(object.configuration.infrastructure_configuration)
+            vis_id = object.name
+            vis_location = object.location
+            vis_environment = object.environment
+            vis_status = object.status
+            vis_health = object.health 
+
+            infra_object = object.configuration.infrastructure_configuration
+            vis_rg = infra_object.app_resource_group
+            print("######VIS1")
+            print(object.system_data)
+            print("######VIS2")
+            print(object.configuration)
+            #print(object.configuration.infrastructure_configuration.os_sap_configuration)
+            print("######VIS3")
+            print(object.configuration.infrastructure_configuration)
+            print(object.configuration.infrastructure_configuration.virtual_machine_configuration)
+            vis_app_vm_size = object.configuration.infrastructure_configuration.virtual_machine_configuration.vm_size
+            print(vis_app_vm_size)
+            print("VIS4")
+            print(object.configuration.software_configuration)
+            vis_software = object.configuration.software_configuration.software_version
+
+            #App
+            #vis_app_vm_size = infra_object.application_server.virtual_machine_configuration.vm_size
+            #vis_app_image = infra_object.application_server.virtual_machine_configuration.image_reference.publisher
+            #vis_app_sku = infra_object.application_server.virtual_machine_configuration.image_reference.sku
+            #vis_app_instance_count = infra_object.application_server.instance_count
+
+            #DB
+            #vis_db_vm_size = infra_object.database_server.virtual_machine_configuration.vm_size
+            #vis_db_image = infra_object.database_server.virtual_machine_configuration.image_reference.publisher
+            #vis_db_sku = infra_object.database_server.virtual_machine_configuration.image_reference.sku
+            #vis_db_instance_count = infra_object.database_server.instance_count
+
+            #vis_info = (vis_id, vis_rg, vis_location, vis_environment, vis_app_vm_size, vis_app_image, vis_app_sku, vis_app_instance_count, vis_db_vm_size, vis_db_image, vis_db_sku, vis_db_instance_count, vis_status, vis_health)
+            vis_info = (vis_id, vis_rg, vis_location, vis_environment, vis_app_vm_size, vis_software, vis_status, vis_health)
+            print(vis_info[4])
+            rg_list.append(vis_info)
+
+            print(rg_list)
+    except Exception as e:
+        return ("Get VIS info failed with error: {}".format(e), os.environ['AZURE_TENANT_ID'], "No available content")
+
+    return rg_list
+    
